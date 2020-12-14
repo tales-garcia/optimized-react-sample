d11bfd7ea32fbb4c6f087e41373eb4dc3f821e88
Tales Garcia <garcia.tales@gmail.com>
2020-12-14 19:04:15 -0300

    feat(styles): apply some styles to post component

--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    height: 280px;
+    width: 100%;
 
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: space-between;
+
+    img {
+        height: 200px;
+    }
 `;
